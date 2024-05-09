7fd4227e34c340e9b5aeac112e802d2136f6dc0a
6 <6@PC-06>
2024-05-09 11:47:39 +0900

    d

--- src/util/View.java
+++ src/util/View.java
@@ -7,7 +7,7 @@ public enum View {
 	ADMIN_PROD_INSERT,		// 상품 추가
 	ADMIN_PROD_DELETE,		// 상품 삭제
 	ADMIN_PROD_UPDATE,		// 상품 변경
-	ADMIN_PROD_LIST,			// 상세 페이
+	ADMIN_PROD_LIST,		// 상세 페이
 	ADMIN_PROD_DETAILE,		// 상품 전체 출력
 	LOGIN					// 로그인
 			
